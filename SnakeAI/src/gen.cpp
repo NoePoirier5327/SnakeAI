65f8f75312f7b8b6e582b2a03ba8b9caaf00de9d
Monophano <89103355+Monophano@users.noreply.github.com>
2026-01-16 19:12:57 +0100

    Début de l'ajout d'un algorithme génétique.

--- SnakeAI/src/gen.cpp
+++ SnakeAI/src/gen.cpp
@@ -5,9 +5,47 @@
 */
 
 #include "gen.hpp"
-#include "ai_engine/neuron.hpp" // importé pour utiliser les fonctions d'aléatoire
+#include "ai_engine/dqnagent.hpp"
+#include "game.hpp"
 
-void genetic_algorithm(std::vector<int> &net_shape, float &learning_rate, float &epsilon, float &gamma, int &batch_size, int &memory_size, float &epsilon_decay, float &epsilon_end, int &convergence_counter, int nb_gen, int &best_score, int &gen_best_score)
+void genetic_algorithm(std::vector<int> &net_shape, float &learning_rate, float &epsilon, float &gamma, int &batch_size, int &memory_size, float &epsilon_decay, float &epsilon_end, int &convergence_counter, int population, int nb_gen, int &best_score, int &gen_best_score, SDL_Renderer *renderer)
 {
+    // On commence par créer la première population d'agents d'apprentissage avec des paramètres d'entrées totalement aléatoires
+    // ainsi que leurs environnements d'apprentissage associés
+    std::vector<DQNAgent *> agents;
+    std::vector<Game *> environnments;
 
+    for (int i = 0; i < population; ++i)
+    {
+        // D'abord l'environnement d'apprentissage
+        environnments.push_back(new Game(renderer));
+
+        // Ensuite les agents
+        std::vector<int> net = {77}; // On a forcément 77 entrées
+
+        int n = int_rng(1, 10); // Il y aura forcément entre 1 et 10 couches cachées
+        for (int j = 0; j < n; ++j)
+            net.push_back(int_rng(1, 256)); // Il y aura forcément entre 1 et 256 neurones par couches cachées
+
+        net.push_back(4); // et on a forcément 4 sorties
+
+        agents.push_back(new DQNAgent(net, rng(0, 1), rng(0,1), rng(0, 1), int_rng(1, 512), int_rng(1, 200000), rng(0, 1), rng(0, 1), int_rng(1, 1000)));
+    }
+
+    // Maintenant, on fait jouer et on entraine les agents sur leurs environnements respectifs
+    // et lorsqu'ils sont tous morts, on recréer le vecteur agents avec de nouveaux agents combinant les caractéristiques des deux derniers meilleurs agents ainsi qu'un nombre aléatoire de caracteristiques aléatoire
+    for (int gen = 0; gen < nb_gen; ++gen)
+    {
+        int nb_actifs = population; // On garde une trace du nombre d'agents qui n'est pas encore mort dans la population courante
+
+        // On fait jouer les agents dans leurs environnements tant qu'ils ne sont pas mort
+        while (nb_actifs > 0)
+        {
+            for (int i = 0; i < population; ++i)
+            {
+                if (environnments[i]->game_over == true)
+                    nb_actifs --;
+            }
+        }
+    }
 }
--- SnakeAI/src/gen.hpp
+++ SnakeAI/src/gen.hpp
@@ -8,6 +8,8 @@
 
 #include "ai_engine/dqnagent.hpp"
 
+#include <SDL2/SDL_render.h>
+
 /**
  * @brief Fonction réalisant un algorithme génétique afin de déterminer les meilleurs paramètres pour entrainer l'agent de snake plus tard
  * @param net_shape: std::vector<int>, paramètre modifié par la fonction pour donner le meilleur nombre de couches cachées avec le meilleur nombre de neurones par couches cachées pour le snake
@@ -19,8 +21,10 @@
  * @param epsilon_decay: float, modifié par la fonction pour avoir le meilleur taux de baisse d'epsilon au cours du temps
  * @param epsilon_end: float, modifié par la fonction pour avoir la meilleure valeur d'epsilon après avoir convergé
  * @param convergence_counter: int, modifié par la fonction pour avoir le meilleur delai de convergence
+ * @param population: int, population par génération d'agents d'apprentissage
  * @param nb_gen: int, détermine combien d'itérations doit réaliser l'algorithme
  * @param best_score: int, modifié par la fonction, contient le meilleur score obtenu par un agent
  * @param gen_best_score: int, modifié par la fonction, contient le numéro de la génération ayant réalisé le meilleur score
+ * @param renderer: SDL_Renderer, fenêtre de rendue sur laquelle afficher les agents en train de s'entrainer
 */
-void genetic_algorithm(std::vector<int> &net_shape, float &learning_rate, float &epsilon, float &gamma, int &batch_size, int &memory_size, float &epsilon_decay, float &epsilon_end, int &convergence_counter, int nb_gen, int &best_score, int &gen_best_score);
+void genetic_algorithm(std::vector<int> &net_shape, float &learning_rate, float &epsilon, float &gamma, int &batch_size, int &memory_size, float &epsilon_decay, float &epsilon_end, int &convergence_counter, int population, int nb_gen, int &best_score, int &gen_best_score, SDL_Renderer *renderer);
